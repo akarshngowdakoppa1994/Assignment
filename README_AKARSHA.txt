6a9a1d2567b1d0f4caa57b909221dc3ba0c99771
akarshngowdakoppa1994 <aakarshagowda@gmail.com>
2014-09-16 18:49:38 +0530

    Assignment_3

--- README_AKARSHA.txt
+++ README_AKARSHA.txt
@@ -8,33 +8,31 @@ Team member's Name:Mahesh Kumar
 Team member's USN :1PI12IS049
 
 Question No.: 3
-Question Title: Write a â€œloginâ€ program for a user that asks the user to enter a user name and a password. The 
+Question Title: Write a “login” program for a user that asks the user to enter a user name and a password. The 
 				use rname and password should have a matching entry in the file which has the data stored in 
 				the following format:
 				username1,password1
 				username2,password2
 				username3,password3
 				...
-				If the user name and password  matches an entry in the file, display a message saying â€œWelcome 
-				<user name>!â€.  If  a  matching  entry  is  not  found,  display  a  message  â€œWrong 
-				user name/password, please try againâ€ and prompt the user to enter his/her credentials again. 
+				If the user name and password  matches an entry in the file, display a message saying “Welcome 
+				<user name>!”.  If  a  matching  entry  is  not  found,  display  a  message  “Wrong 
+				user name/password, please try again” and prompt the user to enter his/her credentials again. 
 				Allow  a  maximum  of  up to  3  consecutive  incorrect  attempts  and  then  the  program  should 
-				terminate  saying  â€œMaximum  attempts  exceededâ€.  Consider  at  least  20  entries  in  the  file 
+				terminate  saying  “Maximum  attempts  exceeded”.  Consider  at  least  20  entries  in  the  file 
 				containing user name and passwords.
 
 				
-->Solution for the above question contains two files asn2_3.c & Data.txt
-->asn2_3.c contains the source code 
+->Solution for the above question contains two files asn3_3.c & Data.txt
+->asn3_3.c contains the source code 
 ->Data.txt is a text file having stored data of user name & password .It is stored in the format (user name,password).We have stored 21 username and corresponing password.
 
-->As per instruction 5 we have asked the user to use the program in the format ./a.out -f <filename> -l <logfilename>.If he fails a message will be displayed indicating the
- valid format
-->we have used getopt and a log file is created automatically when the program is executed.
-->user can enter the valid user name and password  within 3 attempts.If he fails program terminates.
-->every time on taking input it is checked with the data stored in Data.txt file
-->all the parameters of the question & instructions have been covered 
-->in log file the format is :(Input Taken: username password   week month date hour:minutes:seconds year)
-->we have compiled this code using ubuntu.
--
+->we have modified code of asn2_3.c slightly.
+->modified code gives the same output as asn2_3.c and also it prints the last login time of the user if and only if  the login is successful.  
+->format of file input is user name,password,last login time
+->and our code modifies login time in the file after every successful login,which can be accessed on subsequent use.
+
+
+
 
 >thank you
--- asn2_3.c
+++ asn2_3.c
@@ -5,10 +5,12 @@
                            int Access(char *);
                            void Logo(char *);
                            void LogFun(char *l);
+			   void LastLog(FILE *f);
 
 *Description: Log functions logs the activities of the program to a Log file.
               File Process will process the file for validation of username and Password.
-              Access function checks whether the file exists or not
+              Access function checks whether the file exists or not.
+	    : LastLog() function overwrites the login time of user on every successful login.
 			 
 *Returns: FileProcess will return 1 after successful operations else returns 0
           Access returns 1 if file exists, 0 otherwise.
@@ -23,7 +25,8 @@ void Log(char *,char *,char *);
 int FileProcess(char *,char*,char *,char *);
 int Access(char *);
 void Logo(char *);
-void LogFun(char *l);
+void LogFun(char *);
+void LastLog(FILE *);
 
 int main(int argc, char **argv)
 {
@@ -36,15 +39,15 @@ int main(int argc, char **argv)
 	{
   	  	switch(c)
   	  	{
-  			case 'f': fname=optarg;
+  		    case 'f': fname=optarg;
 				  break;
 
 			case 'l': lname=optarg;
 				  break;
 
-			default : printf("Invalid Arguments");
-				  printf("\n\n\n-----------------------------------------------------\n\n\n");
-			          return 1;
+			default: printf("Invalid Arguments");
+				printf("\n\n\n-----------------------------------------------------\n\n\n");
+			         return 1;
 	   	}
 
 	 }
@@ -53,43 +56,42 @@ int main(int argc, char **argv)
 	
 	if(argc>=3)
 	{
-        	if(Access(fname)==1)
-       		{
+        if(Access(fname)==1)
+        {
 	
        
-      			printf("\n\n\n----------------------WELCOME-----------------------\n\n\n");
+          printf("\n\n\n----------------------WELCOME-----------------------\n\n\n");
 
-			while(i==0)
-		   	{   
+		   while(i==0)
+		   {   
 
  
-  			 	printf("Enter User Name\n");
-  			 	scanf("%s",name);
+  			 printf("Enter User Name\n");
+  			 scanf("%s",name);
   
- 			 	printf("\nEnter Password\n");
- 			 	scanf("%s",pass);
+ 			 printf("\nEnter Password\n");
+ 			 scanf("%s",pass);
 	
-		        	Log(name,pass,lname);
-    				i=FileProcess(name,pass,fname,lname);
-      			}
+		        Log(name,pass,lname);
+    			i=FileProcess(name,pass,fname,lname);
+          }
        
-       		}
-       		else
-       		{
-     			printf("\n\nFile Named %s Doesn't Exist\n\n",fname);
-			printf("\n\n\n-----------------------------------------------------\n\n\n");
-		     	return 1;
-	        }
+       }
+       else
+       {
+     	printf("\n\nFile Named %s Doesn't Exist\n\n",fname);
+     	return 1;
+      }
       
        
-   	}
+   }
    
 	else
 	{
 	  	printf("\nInvalid Arguments..!!!\n\nInput Format: ./a.out -f <filename> -l <logfile>\n");
 		printf("\n\n\n-----------------------------------------------------\n\n\n");
 	} 
-	return 0;
+	 return 0;
 }
 
 int FileProcess(char *name,char *pass,char *fname,char *lname)
@@ -97,30 +99,33 @@ int FileProcess(char *name,char *pass,char *fname,char *lname)
 	LogFun(lname);
 	FILE *fp;
 	int i=0;
-	int j,k;
+	int j,k,m;
 	static count=0;
-	char line[40],username[20],password[10];
+	char line[70],username[20],password[10],day[3],month[3],date[2],time[8],year[4];
 
 
 	
-	fp=fopen(fname,"r");
+	fp=fopen(fname,"r+");
   
 	do
   	{
 	  	j=0;
 		k=0;
-		fscanf(fp,"%s",line);
-     	
-     		while(line[j]!=',')
+		m=0;
+		
+		fscanf(fp,"%s %s %s %s %s",line,month,date,time,year);
+     	       
+	
+     	while(line[j]!=',')
 		{
 			username[j]=line[j];
 			j++;
 		}	
-     		username[j]='\0';
+     	username[j]='\0';
      	
 		j++;
     		
-		while(line[j]!='\0')
+		while(line[j]!=',')
 		{
 			password[k]=line[j];
 			j++;
@@ -128,17 +133,31 @@ int FileProcess(char *name,char *pass,char *fname,char *lname)
 		}
 		
 		password[k]='\0';
+		j++;
+		while(line[j]!='\0')
+		{
+			day[m]=line[j];
+			j++;
+			m++;
+		}
+		
+		day[m]='\0';
 			
 	    
-      		if((strcmp(name,username)==0)&&(strcmp(pass,password)==0))
+      	if((strcmp(name,username)==0)&&(strcmp(pass,password)==0))
 	  	{
-        	        printf("\nWelcome %s \n\n",username);	
-			printf("-------------------Have A Nice Day------------------\n\n\n");
-		  	Logo(lname);
-			i=1;
-		  	break; 
+                printf("\nWelcome %s \n\n",username);
+		printf("\nLast Login: %s %s %s %s %s \n\n",day,month,date,time,year);
+				
+		printf("--------------------Have A Nice Day------------------\n\n\n");		
+				LastLog(fp);
+
+			
+		  		Logo(lname);
+				i=1;
+		  		break; 
 		  
-		}
+	    }
 		
 	  	
 	  
@@ -153,14 +172,14 @@ int FileProcess(char *name,char *pass,char *fname,char *lname)
 
   		if(count>=3)
   		{
-			printf("----------------------------------------------------\n");    			
+			printf("-------------------------------------------------------------\n");    			
 			printf("\nSorry Maximum Trials Exceeded..!!!\n\n");
 			Logo(lname);
      			i=1;
   		}
   		else
   		{
-			printf("------------------PLEASE TRY AGAIN------------------\n\n\n");
+			printf("-----------------PLEASE TRY AGAIN----------------\n\n\n");
 	  	}
 	}
   	fclose(fp);
@@ -224,6 +243,19 @@ void LogFun(char *l)
 	 
 }   
 
+void LastLog(FILE *f)
+{
+	
+	
+	fseek(f,-24,SEEK_CUR);
+	
+	time_t rawtime;
+  	struct tm * timeinfo;
+        
+  	time ( &rawtime );
+  	timeinfo = localtime ( &rawtime );
+	fprintf(f,"%s",asctime (timeinfo) );
+}	 
 
 int Access(char *fname)
 {
